3e38af39dd2a202b7c1c9dd46ee3c287ef9cdd75
otito <otitoamuga@gmail.com>
2026-02-13 20:58:46 -0700

    p

--- app/src/main/java/com/example/mrgyroman/FirstFragment.java
+++ app/src/main/java/com/example/mrgyroman/FirstFragment.java
@@ -1,5 +1,13 @@
 package com.example.mrgyroman;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
+import android.widget.TextView;
+
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,17 +18,83 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.example.mrgyroman.databinding.FragmentFirstBinding;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements SensorEventListener {
+    private FragmentFirstBinding binding;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
 
-private FragmentFirstBinding binding;
+    private float[] accelValues = new float[3];
+    private float[] magValues = new float[3];
 
 
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
 
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
 
-@Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            accelValues = event.values.clone();
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            magValues = event.values.clone();
+        }
+
+        float[] R = new float[9];
+        float[] orientation = new float[3];
+
+        if (SensorManager.getRotationMatrix(R, null, accelValues, magValues)) {
+            SensorManager.getOrientation(R, orientation);
+
+            float azimuth = (float) Math.toDegrees(orientation[0]);
+            float pitch = (float) Math.toDegrees(orientation[1]);
+            float roll = (float) Math.toDegrees(orientation[2]);
+
+            binding.textviewFirst.setText(
+                    "Azimuth: " + azimuth +
+                            "\nPitch: " + pitch +
+                            "\nRoll: " + roll
+            );
+        }
+    }
+
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 }
