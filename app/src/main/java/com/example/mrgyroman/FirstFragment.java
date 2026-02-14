0d1a08a86dbdc435b485dc961d0c56c23133d614
otito <otitoamuga@gmail.com>
2026-02-13 20:42:25 -0700

    p

--- app/src/main/java/com/example/mrgyroman/FirstFragment.java
+++ app/src/main/java/com/example/mrgyroman/FirstFragment.java
@@ -14,25 +14,8 @@ public class FirstFragment extends Fragment {
 
 private FragmentFirstBinding binding;
 
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
 
-      binding = FragmentFirstBinding.inflate(inflater, container, false);
-      return binding.getRoot();
 
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonFirst.setOnClickListener(v ->
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
-        );
-    }
 
 @Override
     public void onDestroyView() {
